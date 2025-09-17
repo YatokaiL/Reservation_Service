a9bd79a0334e7710ce558da7c576a0c79163e193
YatokaiL <yatokirigami@gmail.com>
2025-09-17 11:11:50 +0300

    init commit

---
+++ reservation-system/src/test/java/ru/prilepskij/reservation/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prilepskij.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
